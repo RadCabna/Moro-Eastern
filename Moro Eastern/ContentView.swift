75a37706d5ecf817c5094dd9ceb738c6c91a8962
Radcabna <radcabna@gmail.com>
2026-03-12 10:11:59 +0300

    Initial Commit

---
+++ Moro Eastern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moro Eastern
+//
+//  Created by Алкександр Степанов on 12.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moro Eastern/Moro_EasternApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moro_EasternApp.swift
+//  Moro Eastern
+//
+//  Created by Алкександр Степанов on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Moro_EasternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
